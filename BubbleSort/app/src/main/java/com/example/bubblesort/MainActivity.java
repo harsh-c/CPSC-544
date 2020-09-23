e00f78e95f80f175797125a992e0029b993b7a15
harsh-c <harsh_chauhan94@hotmail.com>
2020-09-22 22:57:52 -0700

    Adding additional validations for input array

--- BubbleSort/app/src/main/java/com/example/bubblesort/MainActivity.java
+++ BubbleSort/app/src/main/java/com/example/bubblesort/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.bubblesort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -25,20 +27,30 @@ public class MainActivity extends AppCompatActivity {
         outputNumber = (TextView)findViewById(R.id.outputNumber);
     }
 
-    public void sortButton(View view){
+    public void sortButton(View view) {
         //get the text from the input field
         String[] enteredNumber = inputNumber.getText().toString().split(" ");
         String output = "";
+        // Check for validations
+        // If invalid then show toast message
+        Boolean validationFlag = checkValidation(enteredNumber);
 
-        int[] numberList = new int[enteredNumber.length];
+        if(validationFlag==true)
+        {
+            // Convert the input array to integers
+            int[] numberList = convertToIntArray(enteredNumber);
+            output = printArray(numberList);
 
-        //convert the string to int array
-        for(int i =0; i<enteredNumber.length; i++){
-            numberList[i]= Integer.parseInt(enteredNumber[i]);
+            if(numberList.length > 2 && numberList.length < 9){
+                // Set the input array text
+                inputArray.setText("Input Array: " + output);
+
+                bubbleSort(numberList, numberList.length);
+            }
+            else{
+                toastMessage("Please enter minimum 3 numbers and max 8 numbers between 0 - 9 separated by one space");
+            }
         }
-        output = printArray(numberList);
-        inputArray.setText("Input Array: " + output);
-        bubbleSort(numberList, numberList.length);
     }
 
     private void bubbleSort (int[] input, int n){
@@ -76,4 +88,44 @@ public class MainActivity extends AppCompatActivity {
 
         return output.toString();
     }
+
+    void toastMessage(String message){
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    int[] convertToIntArray(String[] arr){
+        int[] numberList = new int[arr.length];
+
+        //convert the string to int array
+        for (int i = 0; i < arr.length; i++) {
+            try {
+                numberList[i] = Integer.parseInt(arr[i]);
+            } catch (Exception e) {
+                toastMessage("Error converting to integer value");
+            }
+        }
+        return numberList;
+    }
+
+    Boolean checkValidation(String[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            try {
+                int num = Integer.parseInt(arr[i]);
+                if ( num < 0 || num > 9){
+                    toastMessage("Number entered is less than 0 or greater than 9. Please enter numbers between 0 - 9");
+                    return false;
+                }
+            } catch (Exception e) {
+                toastMessage("Please enter numbers between 0 - 9 separated by one space");
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
